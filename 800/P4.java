2c8ebb2c1c9e5bbf15ffdec46221f32e56eee91f
Tarun-kondaveti <tarun.kondaveti@gmail.com>
2025-08-10 13:57:44 +0530

    A Game with Integers

---
+++ 800/P4.java
@@ -0,0 +1,19 @@
+
+// https://codeforces.com/problemset/problem/1899/A - A. Game with Integers
+import java.util.*;
+public class P4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            if(n % 3 == 0){
+                System.out.println("Second");
+            }
+            else{
+                System.out.println("First");
+            }
+        }
+        sc.close();
+    }
+}
