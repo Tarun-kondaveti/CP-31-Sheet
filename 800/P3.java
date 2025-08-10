28ba1e612ff401d61930ef22e728f22577d750b8
Tarun-kondaveti <tarun.kondaveti@gmail.com>
2025-08-10 10:49:04 +0530

    Cover in Water

---
+++ 800/P3.java
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/1900/A
+
+import java.util.*;
+public class P3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            String s = sc.next();
+            int cnt = 0;
+            if(s.contains("...")) {
+                System.out.println(2);
+                continue;
+            }
+            for(int i = 0 ; i < n ; i++){
+                if(s.charAt(i) == '.') cnt++;
+            }
+            System.out.println(cnt);
+        }
+    }
+}
