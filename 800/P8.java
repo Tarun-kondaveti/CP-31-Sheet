93c5950fb31ab24a0d1a995e92d7a915ff5f868d
Tarun-kondaveti <tarun.kondaveti@gmail.com>
2025-08-15 22:18:16 +0530

    How Much Does Daytona Cost?

---
+++ 800/P8.java
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/1878/A - How Much Does Daytona Cost?
+
+import java.util.*;
+public class P8{
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int target = sc.nextInt();
+            boolean flag = false;
+            int[] arr = new int[n];
+            for(int i = 0 ; i < n ; i++){
+                arr[i] = sc.nextInt();
+                if(target == arr[i]) flag = true;
+            }
+            if(flag) System.out.println("YES");
+            else System.out.println("NO");
+        }
+        sc.close();
+    }
+}
