75dd5d08066d892b7e7393616dc86ac4fbccd4f4
Tarun-kondaveti <tarun.kondaveti@gmail.com>
2025-08-16 22:20:34 +0530

    How Much Does Daytona Cost?

--- 800/P8.java
+++ 800/P8.java
@@ -8,13 +8,13 @@ public class P8{
         while(t-- > 0){
             int n = sc.nextInt();
             int target = sc.nextInt();
-            boolean flag = false;
+            boolean f = false;
             int[] arr = new int[n];
             for(int i = 0 ; i < n ; i++){
                 arr[i] = sc.nextInt();
-                if(target == arr[i]) flag = true;
+                if(target == arr[i]) f = true;
             }
-            if(flag) System.out.println("YES");
+            if(f) System.out.println("YES");
             else System.out.println("NO");
         }
         sc.close();
